03199cdc980f84f9598caa3d3711bf98f32b755b
kanshum <kanshum@nagarro.com>
2024-11-25 18:08:53 +0530

    modified app [WIP]

--- Application/src/main/java/com/example/helloworld/HelloWorldController.java
+++ Application/src/main/java/com/example/helloworld/HelloWorldController.java
@@ -1,15 +1,14 @@
-package com.example.helloworld;
+package com.example.springboot;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloWorldController {
 
-    @GetMapping("/")
-    public String helloWorld(Model model) {
-        model.addAttribute("message", "Welcome to the Datadog APM Hello World Application!");
-        return "index"; // This maps to src/main/resources/templates/index.html
-    }
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
 }
